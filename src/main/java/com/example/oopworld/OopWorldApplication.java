838cd9897a536e5c3542ce6d3ede7a6ba29f73aa
heybys <byn0174@gmail.com>
2023-11-28 13:35:44 +0900

    init

---
+++ src/main/java/com/example/oopworld/OopWorldApplication.java
@@ -0,0 +1,13 @@
+package com.example.oopworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OopWorldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oopworld/OopWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oopworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
